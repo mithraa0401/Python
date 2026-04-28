c3ee06d75b1a63196b95128288ce0c43bea9f6d5
mithraa0401 <mithraa0401@gmail.com>
2026-04-28 12:14:22 +0530

    Add files via upload

---
+++ Mithra.py
@@ -0,0 +1,4 @@
+a=int(input("Enter a value a:"))
+b=int(input("Enter a value b:"))
+c=a+b
+print(c)
---
+++ mithra.2.py
@@ -0,0 +1,5 @@
+name="Mithra"
+age=18
+place="tiruchengode"
+college="Svs polytechnic college"
+print(college)
